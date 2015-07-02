19a88cb3891332fe2880f937766b519053e9c0dd
Mordechai <mordechai@Mordechais-MacBook-Pro.local>
2015-07-02 17:18:45 +0300

    working a few examples

---
+++ exer-2.js
@@ -0,0 +1,10 @@
+console.log(process.argv)
+var answer = 0;
+function calcArray(someArray){
+for (var i = 2; i < someArray.length; i++) {
+    answer += Number(someArray[i]);
+};	
+}
+
+calcArray(process.argv)	
+console.log(answer)
